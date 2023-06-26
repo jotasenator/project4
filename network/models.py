d1d902c3fe3e6de5e3dc52c790bf21929d1b975a
Maykel <jotasenator@gmail.com>
2023-06-26 10:00:47 -0400

    Create model Post, Create superuser, Add blue main color

--- network/models.py
+++ network/models.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from django.utils import timezone
+
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, default=None, related_name="post"
+    )
+    created_at = models.DateTimeField(default=timezone.now)
+    text = models.TextField(default="")
