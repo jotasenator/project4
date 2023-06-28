c523bd63a3980a1f7502976ff5546fc4e540c89d
Maykel <jotasenator@gmail.com>
2023-06-28 12:38:28 -0400

    Add behavior in like button if no user is loged in and Elimnate console error by checking if the are #post-form elements

---
+++ network/static/network/likeButtonNotAuth.js
@@ -0,0 +1,19 @@
+console.log( 'likeButtonNotAuth.js loaded' );
+
+
+window.addEventListener( "load", () =>
+{
+    disabledLikeButtons();
+} );
+
+// Disable like buttons
+const disabledLikeButtons = () =>
+{
+    const likeButtons = document.querySelectorAll( '.like-button' );
+    likeButtons.forEach( button =>
+    {
+        button.disabled = true;
+        button.style.cursor = 'default';
+        button.style.opacity = 0.5;
+    } );
+};
